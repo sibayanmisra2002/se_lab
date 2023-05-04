601e029f4ea112b094c198a2c5683646363c6804
Sibayan Misra <sibayanmisra2002@gmail.com>
2023-05-04 08:10:52 +0530

    Add files via upload

---
+++ PHP codes/header.php
@@ -0,0 +1,35 @@
+<?php
+include("link.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table{
+	font-family: arial,sans-serif;
+	border-collapse: collapse;
+	width: 100%;
+}
+
+td, th{
+	border: 1px solid #dddddd;
+	text-align:left;
+	padding:8px;
+}
+
+tr: nth-child(even){
+	background-color: #dddddd;
+	border: 1px solid #dddddd;
+	text-align: left;
+	padding: 8px;
+}
+
+tr:nth_child(even)  {
+	background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+
+<h2> HTML Table </h2>
